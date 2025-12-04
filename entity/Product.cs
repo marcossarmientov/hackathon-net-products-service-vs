39fc1fb7264af8c410eb73d6fc0f5dc84e387844
Marcos Sarmiento <marcossarmiento@outlook.com>
2025-12-04 08:50:11 -0500

    Creacion de Product Entity

--- entity/Product.cs
+++ entity/Product.cs
@@ -2,5 +2,15 @@
 {
     public class Product
     {
+        public int productId { get; set; }
+        public string productName { get; set; }
+        public string productDescription { get; set; }
+        public string sku { get; set; }
+        public double price { get; set; }
+        public double discount { get; set; }
+        public string currency { get; set; }
+        public int stock { get; set; }
+        public double weight { get; set;}
+        public string dimensions { get; set; }
     }
 }
